6f0efbefe535e4cdab0d42ac2d2fc1102c44b6ef
ismail <benammarismail31@gmail.com>
2020-03-30 12:57:41 +0200

    checkstyle

--- src/test/java/uvsq/pglp_3_2/AppTest.java
+++ src/test/java/uvsq/pglp_3_2/AppTest.java
@@ -1,38 +1,82 @@
 package uvsq.pglp_3_2;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.junit.Assert.*;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import java.util.ArrayList;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+
+public class AppTest {
+	
+	@org.junit.Test
+	public void testVendeur(){
+
+		Vendeur v1 = new Vendeur(23);
+    	ArrayList<Employe> employes = new ArrayList<Employe>();
+    	employes.add(v1);
+    	double salaireTotale = 0;
+    	for(Employe emp : employes) {
+    		salaireTotale += emp.calculeSalaire();
+    	}
+    	assertEquals(salaireTotale, 1523, 0.1);
+		
+	}
+	
+	@org.junit.Test
+	public void testVendeurAvecAnciente(){
+
+		Vendeur v1 = new Vendeur(23);
+		v1.ajoutAnné();
+    	ArrayList<Employe> employes = new ArrayList<Employe>();
+    	employes.add(v1);
+    	double salaireTotale = 0;
+    	for(Employe emp : employes) {
+    		salaireTotale += emp.calculeSalaire();
+    	}
+    	assertEquals(salaireTotale, 1543, 0.1);
+		
+	}
+	
+	@org.junit.Test
+	public void testPlusieursVendeur(){
+
+		Vendeur v1 = new Vendeur(23);
+		Vendeur v2 = new Vendeur(0);
+    	v2.ajoutAnné();
+    	
+    	ArrayList<Employe> employes = new ArrayList<Employe>();
+    	employes.add(v1);
+    	employes.add(v2);
+    	double salaireTotale = 0;
+    	for(Employe emp : employes) {
+    		salaireTotale += emp.calculeSalaire();
+    	}
+    	assertEquals(salaireTotale, 3043, 0.1);
+		
+	}
+	
+	@org.junit.Test
+	public void testAjoutManager(){
+
+		Vendeur v1 = new Vendeur(23);
+		Vendeur v2 = new Vendeur(0);
+    	v2.ajoutAnné();
+    	
+    	Manager m1 = new Manager();
+    	m1.ajouterUnePersonne();
+    	m1.ajouterUnePersonne();    	
+    	
+    	ArrayList<Employe> employes = new ArrayList<Employe>();
+    	employes.add(v1);
+    	employes.add(v2);
+    	employes.add(m1);
+    	
+    	double salaireTotale = 0;
+    	for(Employe emp : employes) {
+    		salaireTotale += emp.calculeSalaire();
+    	}
+    	assertEquals(salaireTotale, 4743, 0.1);
+		
+	}
+	
 }
